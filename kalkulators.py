9fb72ac61a440bf51fdf91fb910d773ef4f4fba4
Oskaros-lab <kaminskisoskars@gmail.com>
2024-05-16 09:30:54 +0300

    Update kalkulators.py
    Kalkulators

--- kalkulators.py
+++ kalkulators.py
@@ -3,29 +3,29 @@ from tkinter import*
 Kalkulators=Tk()
 import math
 Kalkulators.title("Kalkulatora projekts")
-
+#========================================================================================================================
 def btnClick(number):
     current=e.get() #nolasa skaitli
     e.delete(0,END) 
     newNumber=str(current)+str(number)
     e.insert(0,newNumber) #  displejs
     return 0
-
+#========================================================================================================================
 def btnCommand(command):
     global number #skaitlis un darbība
     global num1 
-    global mathOp
+    global mathOp#matemātiskais operātors
     mathOp=command
     num1=int(e.get())
     e.delete(0,END)
     return 0
-
+#========================================================================================================================
 def notirit():
-    e.delete(0,END)
+    e.delete(0,END) #nodzēš
     num1=0
     mathOp=""
     return 0
-
+#========================================================================================================================
 def Vienads():
     global num2
     num2=(float(e.get()))
@@ -43,7 +43,7 @@ def Vienads():
     e.delete(0,END)
     e.insert(0,str(result))
     return 0
-
+#========================================================================================================================
 btn0=Button(Kalkulators, text="0", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(0))
 btn1=Button(Kalkulators, text="1", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(1))
 btn2=Button(Kalkulators, text="2", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(2))
@@ -54,14 +54,14 @@ btn6=Button(Kalkulators, text="6", padx="30", pady="20",bg="gray",bd=5,command=l
 btn7=Button(Kalkulators, text="7", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(7))
 btn8=Button(Kalkulators, text="8", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(8))
 btn9=Button(Kalkulators, text="9", padx="30", pady="20",bg="gray",bd=5,command=lambda:btnClick(9))
-
+#========================================================================================================================
 btnReiz=Button(Kalkulators, text="*", padx="30", pady="20", bg="orange",bd=5,command=lambda:btnCommand("*"))
 btnSask=Button(Kalkulators, text="+", padx="30", pady="20",bg="orange",bd=5, command=lambda:btnCommand("+"))
 btnDal=Button(Kalkulators, text="/", padx="30", pady="20",bg="orange",bd=5,command=lambda:btnCommand("/"))
 btnMin=Button(Kalkulators, text="-", padx="30", pady="20",bg="orange",bd=5,command=lambda:btnCommand("-"))
 btnC=Button(Kalkulators, text="C", padx="30", pady="20",bg="grey",bd=5,command=notirit)
 btnVien=Button(Kalkulators, text="=", padx="30", pady="20",bg="grey",bd=5,command=Vienads)
-
+#========================================================================================================================
 btn0.grid(row=5, column=0)
 btn1.grid(row=4, column=0)
 btn2.grid(row=4, column=1)
@@ -78,8 +78,9 @@ btnDal.grid(row=5, column=3)
 btnMin.grid(row=1, column=3)
 btnC.grid(row=5, column=1)
 btnVien.grid(row=5, column=2)
-
+#========================================================================================================================
 e=Entry(Kalkulators, width=15, bd=10, font=("Arial Black",20), justify="center")
 e.grid(row=0, column=0, columnspan=4)
-
+#========================================================================================================================
 Kalkulators.mainloop()
+#========================================================================================================================
